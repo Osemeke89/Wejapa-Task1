aa5370f6d28166e871e00e70a32a58ab97db845a
Osemeke Onyekachukwu <52927146+Osemeke89@users.noreply.github.com>
2020-07-26 21:20:12 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const port = 5000;
+let queryData = "";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World, Welcome to WeJapa Internships");
+
+  if (req.method === "POST") {
+    req.on("data", (data) => {
+      queryData = data;
+    });
+    req.on("end", () => {
+      let Myname = JSON.parse(queryData);
+      return res.end(`Hello ${Myname}, Welcome to WeJapa Internships`);
+    });
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
